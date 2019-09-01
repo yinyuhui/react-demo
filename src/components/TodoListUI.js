a2d71aff655a5713387060decbc85d4e52ba1942
尹玉慧 <yuhui@yinyuhuideMacBook-Pro.local>
2019-09-01 23:30:51 +0800

    将 UI 组件改为无状态组件

--- src/components/TodoListUI.js
+++ src/components/TodoListUI.js
@@ -1,31 +1,58 @@
 import React, { Component } from 'react'
 import { Button, Input, List } from 'antd'
 
-
-export default class TodoListUI extends Component {
-    render() {
-        return (
-            <div style={{ padding: '20px' }}>
-                <label htmlFor="input">请输入内容</label>
-                <Input 
-                    id="input"
-                    value={ this.props.inputValue } 
-                    onChange={ (e) => this.props.handleInput(e) }
-                    style={{ width: 200, margin: '0 10px' }}
-                    placeholder="please input todo info"
-                />
-                <Button type="primary" onClick={ () => this.props.handleClick() }>提交</Button>
-                <List
-                    bordered
-                    style={{ width: 500, marginTop: 20 }}
-                    dataSource={ this.props.list }
-                    renderItem={item => (
-                        <List.Item onClick={ () => this.props.deleteItem(item) }>
-                            {item}
-                        </List.Item>
-                    )}
-                />
-            </div>
-        )
-    }
+const TodoListUI = (props) => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <label htmlFor="input">请输入内容</label>
+            <Input 
+                id="input"
+                value={ props.inputValue } 
+                onChange={ (e) => props.handleInput(e) }
+                style={{ width: 200, margin: '0 10px' }}
+                placeholder="please input todo info"
+            />
+            <Button type="primary" onClick={ () => props.handleClick() }>提交</Button>
+            <List
+                bordered
+                style={{ width: 500, marginTop: 20 }}
+                dataSource={ props.list }
+                renderItem={item => (
+                    <List.Item onClick={ () => props.deleteItem(item) }>
+                        {item}
+                    </List.Item>
+                )}
+            />
+        </div>
+    )
 }
+
+export default TodoListUI
+
+// export default class TodoListUI extends Component {
+//     render() {
+//         return (
+//             <div style={{ padding: '20px' }}>
+//                 <label htmlFor="input">请输入内容</label>
+//                 <Input 
+//                     id="input"
+//                     value={ this.props.inputValue } 
+//                     onChange={ (e) => this.props.handleInput(e) }
+//                     style={{ width: 200, margin: '0 10px' }}
+//                     placeholder="please input todo info"
+//                 />
+//                 <Button type="primary" onClick={ () => this.props.handleClick() }>提交</Button>
+//                 <List
+//                     bordered
+//                     style={{ width: 500, marginTop: 20 }}
+//                     dataSource={ this.props.list }
+//                     renderItem={item => (
+//                         <List.Item onClick={ () => this.props.deleteItem(item) }>
+//                             {item}
+//                         </List.Item>
+//                     )}
+//                 />
+//             </div>
+//         )
+//     }
+// }
